2877a9cd343ccb0d296eeecdc86dc305a6d1b751
lexannal <lt4224uz@go.minnstate.edu>
2025-02-13 08:54:31 -0600

    added custom styles to config

---
+++ config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                green: '#afb98e',
+                lightGreen: '#dfe4bb'
+            },
+            fontFamily: {
+                serifOne: ['Sour Gummy', 'serif'],
+                serifTwo: ['Montserrat', 'serif']
+            }
+        }
+    }
+}
